6a10c1a874807a4194c2c77238f96d90dc190a13
Jay <jay.wang.scorpion@gmail.com>
2019-11-10 14:35:44 +0800

    WIP

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,18 +3,54 @@ import { hot } from "react-hot-loader";
 import { Calendar } from "../../src";
 
 function Item(props: any) {
-    return <div style={props.data.weekday === 0 || props.data.weekday === 6 ? {
-        color: '#999'
-    } : null}> {props.data.day}</div >
+    return (
+        <div
+            style={
+                props.data[props.dayItem.date]
+                    ? {
+                        color: "red"
+                    }
+                    : null
+            }
+        >
+            {props.dayItem.day}
+        </div>
+    );
+
+    // return <div style={props.dayItem.weekday === 0 || props.dayItem.weekday === 6 ? {
+    //     color: '#999'
+    // } : null}> {props.dayItem.day}</div >
 }
 
 function App() {
+    const [data, setData] = React.useState({});
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setData({
+                "2019-10-1": { name: "jay" },
+                "2019-10-2": { name: "jay" }
+            });
+        }, 1000);
+    }, []);
+
+    function fnCallbacka(day: Date | string) {
+        setTimeout(() => {
+            setData({
+                "2019-11-1": { name: "jay" },
+                "2019-11-2": { name: "jay" },
+                "2019-11-3": { name: "jay" }
+            });
+        }, 1000);
+    }
+
     return (
         <Calendar
-            data={[]}
+            data={data}
             firstWeek={1}
             component={Item}
-            currentDay={'2019-10-10'}
+            currentDay={"2019-10-10"}
+            callback={fnCallbacka}
         />
     );
 }
--- src/Calendar/index.tsx
+++ src/Calendar/index.tsx
@@ -1,74 +1,21 @@
 /**
  * @author Jay
- * @date 2019-8-5
+ * @date 2019-11-10
  * @description calendar
  */
 
 import * as React from "react";
-import { getDaysByMonth } from "../utils";
+import { genereateCalendar } from "../utils";
 import "./style";
 
 const weeksStr = ["日", "一", "二", "三", "四", "五", "六"];
 
-/**
- * 构建日历数组
- * @param {*} date
- */
-function fnGenereateCalendar(firstWeek = 0, date = new Date()) {
-    date = new Date(date);
-    // let tempDate = new Date();
-    // const day = date.getDate();
-    // // 当前时间
-    // const tempYear = tempDate.getFullYear();
-    // const tempMonth = tempDate.getMonth();
-    // const tempDay = tempDate.getDate();
-    // // 传入的时间
-    // const dataYear = date.getFullYear();
-    // const dateMonth = date.getMonth();
-    // const dateDay = date.getDate();
-    let arr = [];
-
-    let weekday2;
-    //这个月的第一天是周几 weekday = new Date(date.setDate(1)).getDay();
-    weekday2 = (new Date(date.setDate(1)).getDay() + 7 - firstWeek) % 7;
-    let weekday = new Date(date.setDate(1)).getDay()
-
-    // 这个月有多少天
-    const monthDays = getDaysByMonth(date.getMonth() + 1);
-    console.log(monthDays);
-    // 构建出数组
-    arr = [];
-    // 补全前面上个月天数
-    for (let i = 0; i < weekday2; i++) {
-        arr.push({ day: "" });
-    }
-    // 构建数据
-    for (let i = 1; i <= monthDays; i++) {
-        // vo
-        const vo = {
-            day: i,
-            weekday: weekday,
-            // today: i === tempDay,
-            choose: false
-        };
-        // 添加进数组
-        arr.push(vo);
-
-        weekday++;
-        if (weekday == 7) {
-            weekday = 0;
-        }
-
-    }
-
-    return arr;
-}
-
 interface IProps {
-    data: Array<object>;
+    data: object;
     currentDay?: Date | String;
     firstWeek?: number;
     component?: any;
+    callback?: Function;
 }
 
 /**
@@ -79,39 +26,45 @@ export class Calendar extends React.Component<IProps, any> {
     constructor(props: IProps) {
         super(props);
         this.state = {
-            data: [],
+            days: [],
             currentDay: props.currentDay || new Date(),
             weeks: [0, 1, 2, 3, 4, 5, 6] // 星期
         };
     }
 
+    // static getDerivedStateFromProps(props: any, state: any) {
+    //     console.log('getDerivedStateFromProps', props, state)
+    // }
+
     componentDidMount() {
         const { firstWeek } = this.props;
         let { weeks, currentDay } = this.state;
-        const data = fnGenereateCalendar(firstWeek, currentDay);
-
+        const days = genereateCalendar(firstWeek, currentDay);
         weeks = weeks.concat(weeks.splice(0, firstWeek));
-        this.setState({ data, weeks });
+        this.setState({ days, weeks });
     }
 
     fnChangeMonth = (e: any) => {
-        // console.log('flag', e.target.dataset.flag);
         let { currentDay } = this.state;
-        const { firstWeek } = this.props;
+        const { firstWeek, callback } = this.props;
         currentDay = new Date(currentDay)
         if (e.target.dataset.flag === 'prev') {
             currentDay.setMonth(currentDay.getMonth() - 1)
         } else {
             currentDay.setMonth(currentDay.getMonth() + 1)
         }
-        const data = fnGenereateCalendar(firstWeek, currentDay);
-        this.setState({ currentDay, data })
+        const days = genereateCalendar(firstWeek, currentDay);
+        this.setState({ currentDay, days })
+
+        // callback
+        callback && callback(currentDay)
+
     }
 
     render() {
-        let { data, weeks, currentDay } = this.state;
+        let { days, weeks, currentDay } = this.state;
         currentDay = new Date(currentDay);
-        const { component: Component } = this.props;
+        const { component: Component, data } = this.props;
         return (
             <div className="calender">
                 <div className="title">
@@ -119,20 +72,20 @@ export class Calendar extends React.Component<IProps, any> {
                     <span className='currentDay'>{currentDay.getFullYear() + '-' + (currentDay.getMonth() + 1)}</span>
                     <span onClick={this.fnChangeMonth} data-flag='next'>&gt;</span>
                 </div>
-                {/* 星期 */}
+                {/* week */}
                 <div className="week">
                     {weeks &&
                         weeks.map((item: number, index: number) => (
                             <div key={index}>{weeksStr[item]}</div>
                         ))}
                 </div>
-                {/* 数据 */}
+                {/* days */}
                 <div className="content">
-                    {data &&
-                        data.map(
+                    {days &&
+                        days.map(
                             (item: { day: React.ReactNode }, index: string | number) => (
                                 <div key={index} className="item">
-                                    {Component ? <Component day={item.day} data={item} /> : item.day}
+                                    {Component ? <Component day={item.day} dayItem={item} data={data} /> : item.day}
                                 </div>
                             )
                         )}
